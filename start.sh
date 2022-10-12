29b477c3428983c07825cc6b076f5e3d61c39358
Sam Korn <korn94sam@gmail.com>
2022-10-12 11:52:25 -0600

    still some nagging issues with image persistance

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 prisma db push
 prisma db seed
 mkdir -p /data/images
-ln -sfn /data/images /app/python-backend/images
-cp -n $APPDIR/frontend/public/tabconf.png $APPDIR/python-backend/images/latest.png
+
+[ ! -d /app/python-backend/images/ ] && \
+    ln -sfn /data/images /app/python-backend/images
+    cp -n $APPDIR/frontend/public/tabconf.png $APPDIR/python-backend/images/latest.png
+
+
 gunicorn backend:app --bind 0.0.0.0:8080
